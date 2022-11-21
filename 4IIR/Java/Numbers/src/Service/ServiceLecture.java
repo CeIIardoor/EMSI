4bd1da8348adc59615c0df870948bbcae0f72153
CeIIardoor <cellardoorweb@gmail.com>
2022-11-21 13:17:36 +0100

    sign fix

--- 4IIR/Java/Numbers/src/Service/ServiceLecture.java
+++ 4IIR/Java/Numbers/src/Service/ServiceLecture.java
@@ -13,7 +13,7 @@ public class ServiceLecture {
                         Double.parseDouble(s.substring(s.indexOf("+") + 1, s.indexOf("i"))));
             } else if (s.contains("-")) {
                 return new Complexe(Double.parseDouble(s.substring(0, s.indexOf("-"))),
-                        Double.parseDouble(s.substring(s.indexOf("-") + 1, s.indexOf("i"))));
+                        - Double.parseDouble(s.substring(s.indexOf("-") + 1, s.indexOf("i"))));
             }
         }
         if (s.contains(".")) {
